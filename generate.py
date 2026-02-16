bdca13f6adb51aa4ef5fc8e6918f72486f946b60
Nayshal Kerai <Nayshalk@gmail.com>
2026-02-16 11:58:06 +0000

    Update generate.py

--- generate.py
+++ generate.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from ics import Calendar, Event
 from datetime import datetime
+import re
 
 URL = "https://www.wembleystadium.com/events"
 
@@ -10,30 +11,28 @@ soup = BeautifulSoup(r.text, "html.parser")
 
 cal = Calendar()
 
-for item in soup.select("a[href*='/events/']"):
-    title = item.get_text(strip=True)
+events_found = 0
 
-    # Attempt to find nearby date text
-    parent = item.find_parent()
-    date_text = parent.get_text(" ", strip=True)
-
-    # Very basic date extraction (Wembley uses formats like "22 Mar 2026")
-    import re
-    match = re.search(r"\d{1,2}\s+\w+\s+\d{4}", date_text)
+for text in soup.stripped_strings:
+    match = re.search(r"(\d{1,2}\s+\w+\s+\d{4})", text)
 
     if match:
         try:
-            date = datetime.strptime(match.group(), "%d %b %Y")
+            date = datetime.strptime(match.group(1), "%d %b %Y")
 
             e = Event()
-            e.name = title
+            e.name = "Wembley Stadium Event"
             e.begin = date
             e.duration = {"hours": 3}
 
             cal.events.add(e)
+            events_found += 1
 
         except:
             pass
 
+# ALWAYS write the file (even if empty)
 with open("wembley.ics", "w") as f:
     f.writelines(cal)
+
+print(f"Created calendar with {events_found} events")
